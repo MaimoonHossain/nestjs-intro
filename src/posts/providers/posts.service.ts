29fc2c30d90f940cb13a9e3fbc5676e1181f5061
MaimoonHossain <ashikasitosh740@gmail.com>
2025-09-29 15:41:26 +0600

    soft delete tags done

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -5,6 +5,8 @@ import { Repository } from 'typeorm';
 import { Post } from '../post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MetaOption } from 'src/meta-options/meta-option.entity';
+import { TagsService } from 'src/tags/providers/tags.service';
+import { PatchPostDto } from '../dtos/patch-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -25,8 +27,13 @@ export class PostsService {
      */
     @InjectRepository(MetaOption)
     private readonly metaOptionsRepository: Repository<MetaOption>,
+
+    /**
+     * Inject TagsService
+     */
+    private readonly tagsService: TagsService,
   ) {}
- 
+
   /**
    * Method to create a new post
    */
@@ -34,12 +41,18 @@ export class PostsService {
     // Find the author from database based on authorId
     let author = await this.usersService.findOneById(createPostDto.authorId);
 
+    // Find the tags from database based on tagsId
+    let tags = await this.tagsService.findMultipleTags(
+      createPostDto.tags as any,
+    );
+
     const post = this.postsRepository.create({
       ...createPostDto,
       metaOptions: createPostDto.metaOptions
         ? (createPostDto.metaOptions as any)
         : undefined,
       author: author ? author : undefined,
+      tags: tags ? tags : undefined,
     });
 
     return await this.postsRepository.save(post);
@@ -47,10 +60,37 @@ export class PostsService {
 
   public async findAll() {
     let posts = await this.postsRepository.find({
-      relations: ['author', 'metaOptions'],
+      relations: ['author', 'metaOptions', 'tags'],
     });
 
     return posts;
+  }
+
+  public async update(patchPostDto: PatchPostDto) {
+    // Find the Tags
+    let tags = await this.tagsService.findMultipleTags(
+      patchPostDto.tags as any,
+    );
+    // Find the Post
+    let post = await this.postsRepository.findOneBy({
+      id: patchPostDto.id,
+    });
+    // Update the properties of the post
+    if (post) {
+      post.title = patchPostDto.title ?? post.title;
+      post.content = patchPostDto.content ?? post.content;
+      post.status = patchPostDto.status ?? post.status;
+      post.postType = patchPostDto.postType ?? post.postType;
+      post.slug = patchPostDto.slug ?? post.slug;
+      post.featuredImageUrl =
+        patchPostDto.featuredImageUrl ?? post.featuredImageUrl;
+      post.publishOn = patchPostDto.publishOn ?? post.publishOn;
+      // Assign the new tags
+      post.tags = tags;
+
+      // Save the post and return
+      return await this.postsRepository.save(post);
+    }
   }
 
   public async delete(id: number) {
--- src/tags/providers/tags.service.ts
+++ src/tags/providers/tags.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Tag } from '../tag.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { CreateTagDto } from '../dtos/create-tag.dto';
 
 @Injectable()
@@ -15,5 +15,27 @@ export class TagsService {
   public async create(createTagDto: CreateTagDto) {
     let newTag = this.tagsRepository.create(createTagDto);
     return await this.tagsRepository.save(newTag);
+  }
+
+  public async findMultipleTags(tags: number[]) {
+    let results = await this.tagsRepository.find({
+      where: {
+        id: In(tags),
+      },
+    });
+
+    return results;
+  }
+
+  public async delete(id: number) {
+    await this.tagsRepository.delete(id);
+
+    return { message: 'Tag deleted successfully' };
+  }
+
+  public async softRemove(id: number) {
+    await this.tagsRepository.softDelete(id);
+
+    return { message: 'Tag deleted successfully' };
   }
 }
