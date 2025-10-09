786c29b7c9ea262efd3542be37cc365de4dd263c
MaimoonHossain <ashikasitosh740@gmail.com>
2025-10-09 14:19:53 +0600

    12 - user authentication done

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,19 +2,22 @@ import { IsInt } from 'class-validator';
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
 import { SignInDto } from '../dtos/signin.dto';
+import { SignInProvider } from './sign-in.provider';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(forwardRef(() => UsersService))
     private readonly usersService: UsersService,
+
+    /*
+     * Inject signInProvider
+     */
+    private readonly signInProvider: SignInProvider,
   ) {}
 
-  public signIn(signInDto: SignInDto) {
-    // Find the user using email ID
-    // Throw an expection if user is not found
-    // Compare password to the hash
-    // Send confirmation
+  public async signIn(signInDto: SignInDto) {
+    return await this.signInProvider.signIn(signInDto);
   }
 
   public isAuth() {
---
+++ src/auth/providers/sign-in.provider.ts
@@ -0,0 +1,84 @@
+import { UsersService } from 'src/users/providers/users.service';
+import { SignInDto } from './../dtos/signin.dto';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+  RequestTimeoutException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { HashingProvider } from './hashing.provider';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+import type { ConfigType } from '@nestjs/config';
+
+@Injectable()
+export class SignInProvider {
+  constructor(
+    /*
+     * Injecting usersService
+     */
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+
+    /*
+     * Inject hashingProvider
+     */
+    private readonly hashingProvider: HashingProvider,
+
+    /*
+     * Inject JWT Service
+     */
+    private readonly jwtService: JwtService,
+
+    /*
+     * Inject JWT Service
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  public async signIn(signInDto: SignInDto) {
+    // Find the user using email ID
+    // Throw an expection if user is not found
+    let user = await this.usersService.findOneByEmail(signInDto.email);
+
+    if (!user) {
+      throw new NotFoundException('User not found!!');
+    }
+
+    // Compare password to the hash
+    let isEqual: boolean = false;
+
+    try {
+      isEqual = await this.hashingProvider.comparePassword(
+        signInDto.password,
+        user.password,
+      );
+    } catch (error) {
+      throw new RequestTimeoutException(error, {
+        description: 'Could not compare passwords',
+      });
+    }
+
+    if (!isEqual) {
+      throw new UnauthorizedException('Incorrect Passwored');
+    }
+
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.accessTokenTtl,
+      },
+    );
+
+    return accessToken;
+  }
+}
